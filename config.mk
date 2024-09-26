60622ba0c1d5f212d92bebeea90a6786d7e0af9d
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-09-26 23:27:22 +0000

    custom Makefile that builds local datascience jupyterlab image

--- config.mk
+++ config.mk
@@ -7,3 +7,26 @@ makefile_dir := $(shell dirname $(makefile_path))
 GEOSPATIAL_DIR := $(makefile_dir)/geospatial
 
 GEOSPATIAL_LATEST_DIR := $(GEOSPATIAL_DIR)/latest
+
+DOCKER_USERNAME := hagan
+
+LOCAL_HUB ?= localhost:5000
+LOCAL_URI ?= /vice/jupyter
+HARBOR_HUB ?= harbor.cyverse.org
+HARBOR_URI ?= /vice/jupyter
+
+JLDS_LATEST_DIR := 4.2.5
+JLDS_LATEST_SRC_URI := quay.io/jupyter/
+JLDS_LATEST_SRC_NAME := datascience-notebook
+JLDS_LATEST_SRC_TAG := lab-4.2.5
+
+JLDS_LABEL := jupyterlab-datascience
+JLDS_LATEST_LOCAL_DST_URI := /vice/jupyter
+JLDS_LATEST_LOCAL_DST_NAME := datascience
+JLDS_LATEST_HARBOR_DST_URI := /vice/jupyter
+JLDS_LATEST_HARBOR_DST_NAME := datascience
+JLDS_LATEST_DST_TAG := 4.2.5
+
+
+DOCKER_ENV_VARS := \
+			--env IPLANT_USER
